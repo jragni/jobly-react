e0c5262355ba064626dda486f35693cb9eaa0a9c
Ray Agni <jagni001@ucr.edu>
2021-10-04 00:26:08 -0700

    feat: added login and signup buttons

--- jobly-frontend/src/homepage/Homepage.js
+++ jobly-frontend/src/homepage/Homepage.js
@@ -1,25 +1,45 @@
 import React from "react";
+import "./Homepage.css";
+import { Link } from "react-router-dom";
+
+/** Homepage
+ * The landing page (root) of the application. User can log-in and sign up if
+ * not authenticated. If authenticated, the user can view companies, jobs, their profile,
+ * or logout.
+ */
 
 function Homepage() {
-  function showUserLoggedInView(props) {
-    return <div className="isLoggedIn"></div>;
+  function showUserNotLoggedInView() {
+    return (
+      <div className="btn-group">
+        <Link className="btn btn-primary" to="/signup">
+          Sign Up
+        </Link>
+        <Link className="btn btn-secondary" to="/login">
+          Log In
+        </Link>
+      </div>
+    );
   }
 
-  function showUserNotLoggedInView(props) {
+  function showUserLoggedInView() {
     return (
-      <div className="isNotLoggedIn">
-        <h1>
-          <strong>Jobly</strong>
-        </h1>
-        <h4 className="text-muted">All the jobs. One convenient place.</h4>
+      <div className="isLoggedIn">
+        <h1>Welcome, user</h1>
       </div>
     );
   }
 
   return (
-    <div className="Homepage">
-      {/* Will add authentication layer after */}
-      {true ? showUserNotLoggedInView() : showUserLoggedInView()}
+    <div className="Homepage container">
+      <main className="jumbotron start-50 top-50">
+        <h1>
+          <strong>Jobly</strong>
+        </h1>
+        <h4 className="text-muted">All the jobs. One convenient place.</h4>
+        {/* TODO: Will add authentication layer after */}
+        {true ? showUserNotLoggedInView() : showUserLoggedInView()}
+      </main>
     </div>
   );
 }
--- jobly-frontend/src/nav-routes/Nav.js
+++ jobly-frontend/src/nav-routes/Nav.js
@@ -4,62 +4,79 @@ import "./Nav.css";
 
 /**
  * Nav
- * Navigation Bar
+ * Navigation Bar for Jobly App.
  */
+
+// TODO: ADD links for NavLinks
 function Nav(props) {
+  /* Function that shows navbar features for users that are not lgogged in */
+  function showLoggedOutView() {
+    return (
+      <div>
+        <ul className="navbar-nav me-auto">
+          {/* TODO: Add links to sign up forms when forms completed */}
+          <li className="nav-item">
+            <NavLink className="nav-link" to="">
+              Sign Up
+            </NavLink>
+          </li>
+
+          <li className="nav-item">
+            <NavLink className="nav-link" to="">
+              Log In
+            </NavLink>
+          </li>
+        </ul>
+      </div>
+    );
+  }
+
+  function showLoggedInView() {
+    return (
+      <ul className="navbar-nav me-auto">
+        <li className="nav-item">
+          <NavLink to="" className="nav-link">Companies</NavLink>
+        </li>
+        <li className="nav-item">
+          <NavLink to="" className="nav-link">Jobs</NavLink>
+        </li>
+
+        {/* TODO: Add Profile Link */}
+        <li className="nav-item">
+          <NavLink to="" className="nav-link">Profile</NavLink>
+        </li>
+
+        {/* TODO: Add Logout button */}
+        {/* TODO: Add logout w/ template */}
+        <li to="" className="nav-item">Logout as User</li>
+      </ul>
+    );
+  }
+
   return (
     <nav className="Nav navbar navbar-expand-lg navbar-dark bg-primary">
       <div className="container-fluid">
-          <NavLink to="/" className="navbar-brand">
-              Jobly
-            </NavLink>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarColor01" aria-controls="navbarColor01" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-                </button>
+        <NavLink to="/" className="navbar-brand">
+          Jobly
+        </NavLink>
+        <button
+          class="navbar-toggler"
+          type="button"
+          data-bs-toggle="collapse"
+          data-bs-target="#navbarColor01"
+          aria-controls="navbarColor01"
+          aria-expanded="false"
+          aria-label="Toggle navigation"
+        >
+          <span class="navbar-toggler-icon"></span>
+        </button>
       </div>
-      <ul className='navbar-nav me-auto'>
-        {/* TODO: Add links to sign up forms when forms completed */}
-
-        <li className='nav-item'>
-          <NavLink className='nav-link' to="">Sign Up</NavLink>
-        </li>
-
-        <li className='nav-item'>
-          <NavLink className='nav-link' to="">Log In</NavLink>
-        </li>
-      </ul>
+      <div className="collapse navbar-collapse">
+        {/* TODO: add a logged in view  */}
+        {false ? showLoggedInView() : showLoggedOutView() }
+      </div>
     </nav>
   );
 }
 
 export default Nav;
-
-/**
-<nav class="navbar navbar-expand-lg navbar-dark bg-primary">
-  <div class="container-fluid">
-    <a class="navbar-brand" href="#">Navbar</a>
-    <div class="collapse navbar-collapse" id="navbarColor01">
-      <ul class="navbar-nav me-auto">
-        <li class="nav-item">
-          <a class="nav-link active" href="#">Home
-            <span class="visually-hidden">(current)</span>
-          </a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Features</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">Pricing</a>
-        </li>
-        <li class="nav-item">
-          <a class="nav-link" href="#">About</a>
-        </li>
-      </ul>
-      <form class="d-flex">
-        <input class="form-control me-sm-2" type="text" placeholder="Search">
-        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Search</button>
-      </form>
-    </div>
-  </div>
-</nav>
-*/
